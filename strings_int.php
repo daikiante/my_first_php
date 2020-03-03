35f55b76b92a25fbd7a659751a6b35481e6bc8f8
daikiante <daikiante@gmail.com>
2020-03-03 16:37:52 +0900

    my first PHP

---
+++ strings_int.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+<!-- 文字列を表示する -->
+<?php echo 'Hello World!!'; ?>
+
+<!-- HTMLタグを使う -->
+<?php echo '<h2>Hello World!!</h2>'; ?>
+
+<!-- 計算する -->
+<?php echo 5 + 3; ?>
+
+<!-- 文字列を連結する ドットで連結する -->
+<?php
+    $name = '名無し太郎';
+    
+    echo 'こんにちは！'.$name;
+    
+  ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
